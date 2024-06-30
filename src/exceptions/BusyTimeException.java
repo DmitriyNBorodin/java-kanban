a6dc47c7ce2afcf76d4e7a73843874cc58ccb7d3
DmitriyNBorodin <166409854+DmitriyNBorodin@users.noreply.github.com>
2024-06-30 13:49:19 +0300

    Update BusyTimeException.java

--- src/exceptions/BusyTimeException.java
+++ src/exceptions/BusyTimeException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class BusyTimeException extends RuntimeException{
+public class BusyTimeException extends RuntimeException {
     public BusyTimeException(String message) {
         super(message);
         System.out.println(message);
